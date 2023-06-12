83799d5b387651a661ccf82e7b15dfbaa1f7d3ae
bigttys0 <28068085+bigttys0@users.noreply.github.com>
2023-06-11 22:25:20 -0700

    Create all_close.py

---
+++ all_close.py
@@ -0,0 +1,37 @@
+
+#Below closes all servos
+    kit.servo[0].angle = 0
+    sleep(.2)
+    kit.servo[1].angle = 0
+    sleep(.2)
+    kit.servo[2].angle = 0
+    sleep(.2)
+    kit.servo[3].angle = 0
+    sleep(.2)
+
+    kit.servo[4].angle = 0
+    sleep(.2)
+    kit.servo[5].angle = 0
+    sleep(.2)
+    kit.servo[6].angle = 0
+    sleep(.2)
+    kit.servo[7].angle = 0
+    sleep(.2)
+
+    kit.servo[8].angle = 0
+    sleep(.2)
+    kit.servo[9].angle = 0
+    sleep(.2)
+    kit.servo[10].angle = 0
+    sleep(.2)
+    kit.servo[11].angle = 0
+    sleep(.2)
+
+    kit.servo[12].angle = 0
+    sleep(.2)
+    kit.servo[13].angle = 0
+    sleep(.2)
+    kit.servo[14].angle = 0
+    sleep(.2)
+    kit.servo[15].angle = 0
+    sleep(.2)
